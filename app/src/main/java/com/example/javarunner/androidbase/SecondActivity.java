589e87c23267df6a3ce332183d786d10a43514d9
Dmitry Bystrov <dapper.chita@gmail.com>
2018-05-21 23:54:21 +0900

    home_work_3

--- app/src/main/java/com/example/javarunner/androidbase/SecondActivity.java
+++ app/src/main/java/com/example/javarunner/androidbase/SecondActivity.java
@@ -6,12 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
 
-public class HelloActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hello);
+        setContentView(R.layout.activity_second);
 
         String instanceState;
 
