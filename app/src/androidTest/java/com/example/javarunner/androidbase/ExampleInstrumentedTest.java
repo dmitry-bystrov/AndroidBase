37189dea5ec07fb1980aafe94ae43030726a08f6
Dmitry Bystrov <dapper.chita@gmail.com>
2018-05-21 16:30:46 +0900

    misc

--- app/src/androidTest/java/com/example/javarunner/androidbase/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/javarunner/androidbase/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.javarunner.androidlesson1", appContext.getPackageName());
+        assertEquals("com.example.javarunner.androidbase", appContext.getPackageName());
     }
 }
